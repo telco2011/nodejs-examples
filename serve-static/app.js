40f2ae0e105159af7877a7dbb57ff8b599059c0a
dlpezfelguera <davidlopez.david@gmail.com>
2014-11-05 19:57:34 +0100

    Not expired cache.

--- serve-static/app.js
+++ serve-static/app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-//app.use(express.static(__dirname + '/public'));
-app.use('/static', express.static(__dirname + '/public', { maxAge: oneDay }));
+app.use(express.static(__dirname + '/public'));
+//app.use('/static', express.static(__dirname + '/public', { maxAge: oneDay }));
 
 app.listen(process.env.PORT || 3000);
 console.log("Server listen on http://localhost:3000");
